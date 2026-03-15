e85c46796d15b4dd91ef0be8bd09c1c15014fd99
Frince Louie L. Nacion <louienacion9@gmail.com>
2026-03-15 15:00:00 +0800

    Add endpoint: get_similar_movies

---
+++ endpoints/get_similar_movies.php
@@ -0,0 +1,24 @@
+<?php
+require_once 'providers/_2embed.php';
+header('Content-Type: application/json');
+
+// GET request only
+if ($_SERVER['REQUEST_METHOD'] !== 'GET') {
+    http_response_code(405);
+    echo json_encode(['success' => false, 'message' => 'Method not allowed']);
+    exit();
+}
+
+$movie_id = $_GET['id'] ?? null;
+$page = $_GET['page'] ?? 1;
+
+if (!$movie_id) {
+    http_response_code(400);
+    echo json_encode(['success' => false, 'message' => 'Movie ID is required']);
+    exit();
+}
+
+$url = _2embed::get_similar_movies($movie_id, $page);
+$movie = json_decode(file_get_contents($url), true);
+
+echo json_encode(['success' => true, 'movies' => $movie]);
