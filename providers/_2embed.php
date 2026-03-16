1a95fa3d5eb3e179e7e7b91e300700acb500f54d
Frince Louie L. Nacion <louienacion9@gmail.com>
2026-03-16 21:42:04 +0800

    Update: _2embed documentation-comments

--- providers/_2embed.php
+++ providers/_2embed.php
@@ -1,15 +1,29 @@
 <?php
 class _2embed {
+    /** Uses TMDB or IMDB ID for movie ID
+     * Returns a json response with the movie details and the streaming links (embed)
+     */
     static function get_movie($movie_id) {
         $url = 'https://api.2embed.cc/movie?imdb_id=%s';
         return sprintf($url, $movie_id);
     }
 
+    /**
+     * Returns a json response with the trending movies by page (20 movies per page)
+     * page - page number (default: 1)
+     * results[] - array of movies
+     */
     static function get_trending_movies($page = 1) {
         $url = 'https://api.2embed.cc/trending?page={%d}';
         return sprintf($url, $page);
     }
 
+    /**
+     * Returns a json response with similar movies by page (20 movies per page)
+     * movie_id - the IMDB/TMDB ID of the movie to find similar movies for
+     * page - page number (default: 1)
+     * results[] - array of similar movies
+     */
     static function get_similar_movies($movie_id, $page = 1) {
         $url = 'https://api.2embed.cc/similar?imdb_id=%s&page=%d';
         return sprintf($url, $movie_id, $page);
