080303a594c6b97a5626718581abf74493f22839
Frince Louie L. Nacion <louienacion9@gmail.com>
2026-03-19 23:06:27 +0800

    Add model: show

---
+++ models/show.php
@@ -0,0 +1,8 @@
+<?php
+class Show {
+    public static function get_aggregated_show($show_id){
+        // Similar implementation to Movie::get_aggregated_movie but for shows
+        // This is a placeholder and should be implemented similarly to the Movie class
+        return json_encode(['error' => 'Not implemented yet']);
+    }
+}
