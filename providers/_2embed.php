227d027bfb581024d5ba650156f2d1db0e16ec3d
Frince Louie L. Nacion <louienacion9@gmail.com>
2026-03-20 14:45:13 +0800

    Update: added json extraction logic and error handling to get_trending_movies and get_similar_movies methods

--- providers/_2embed.php
+++ providers/_2embed.php
@@ -19,7 +19,7 @@ class _2embed {
         if (isset($movie['error'])) {
             throw new Error('Movie not found in primary provider');
         }
-        
+
         return $movie_json;
     }
 
@@ -30,7 +30,21 @@ class _2embed {
      */
     static function get_trending_movies($page = 1) {
         $url = 'https://api.2embed.cc/trending?page={%d}';
-        return sprintf($url, $page);
+
+        $movies_json = @file_get_contents($url);
+        if ($movies_json === false) {
+            throw new Error('Failed to fetch movie data from primary provider');
+        }
+
+        $trending_movies = json_decode($movies_json, true);
+        if (json_last_error() !== JSON_ERROR_NONE) {
+            throw new Error('Invalid JSON from primary provider: ' . json_last_error_msg());
+        }
+
+        if (isset($trending_movies['error'])) {
+            throw new Error('Movie not found in primary provider');
+        }
+        return $trending_movies;
     }
 
     /**
@@ -41,6 +55,20 @@ class _2embed {
      */
     static function get_similar_movies($movie_id, $page = 1) {
         $url = 'https://api.2embed.cc/similar?imdb_id=%s&page=%d';
-        return sprintf($url, $movie_id, $page);
+
+        $movies_json = @file_get_contents($url);
+        if ($movies_json === false) {
+            throw new Error('Failed to fetch movie data from primary provider');
+        }
+
+        $similar_movies = json_decode($movies_json, true);
+        if (json_last_error() !== JSON_ERROR_NONE) {
+            throw new Error('Invalid JSON from primary provider: ' . json_last_error_msg());
+        }
+
+        if (isset($similar_movies['error'])) {
+            throw new Error('Movie not found in primary provider');
+        }
+        return $similar_movies;
     }
 }
