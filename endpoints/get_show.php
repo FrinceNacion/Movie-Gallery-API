76458c65ebc9078e7ac069c0a25dc5794cda1f68
Frince Louie L. Nacion <louienacion9@gmail.com>
2026-03-22 18:21:02 +0800

    Add endpoint: get_show

---
+++ endpoints/get_show.php
@@ -0,0 +1,22 @@
+<?php
+require_once '../models/show.php';
+header('Content-Type: application/json');
+
+// GET request only
+if ($_SERVER['REQUEST_METHOD'] !== 'GET') {
+    http_response_code(405);
+    echo json_encode(['error' => 'Method not allowed']);
+    exit;
+}
+$show_id = $_GET['id'] ?? null;
+
+if (!$show_id) {
+    http_response_code(400);
+    echo json_encode(['success' => false, 'message' => 'Show ID is required']);
+    exit();
+}
+
+$url = Show::get_aggregated_show($show_id);
+$show = json_decode($url, true);
+
+echo json_encode(['success' => true, 'show' => $show]);
